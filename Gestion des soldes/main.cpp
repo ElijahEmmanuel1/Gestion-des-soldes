60049fe300d6211dee4c08b6f11868f36da11f91
ElijahEmmanuel1 <bodipoobiangelijah@gmail.com>
2022-11-23 12:56:12 +0100

    Initial Commit

---
+++ Gestion des soldes/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Gestion des soldes
+//
+//  Created by Elijah Loïc BODIPO OBIANG on 23/11/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
